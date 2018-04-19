650fdaffa848fd4a4132831606a1e0821b78cf8e
heaven0sky <286076057@qq.com>
2018-04-19 14:58:46 +0800

    'mogumiao'

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,35 +1,88 @@
+/*
+ Copyright (c) 2011 Shyc2001 (http://twitter.com/shyc2001)
+ This work is based on:
+ *"Switchy! Chrome Proxy Manager and Switcher" (by Mohammad Hejazi (mohammadhi at gmail d0t com))
+ *"SwitchyPlus" by @ayanamist (http://twitter.com/ayanamist)
+ This file is part of SwitchySharp.
+ SwitchySharp is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ SwitchySharp is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with SwitchySharp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var api_url = "http://piping.mogumiao.com/proxy/api/get_ip_bs?appKey=019287c4b45146ea9cbcb8d2b6afc885&count=10&expiryDate=0&format=2"
+var ips = [];
 var cur_tab;
-var ly_tab;
 function init() {
+    get_ips();
     chrome.tabs.create({url: "http://www.hao123.com/?tn=90384165_hao_pg"}, function(tab){
         cur_tab = tab.id;
     });
 
-    chrome.tabs.create({url: "http://tplogin.cn/"}, function(tab){
-        ly_tab = tab.id;
-    });
-    setInterval(run,20000);
-    //setInterval(reload, 5000);
+    setInterval(run, 40000);
 }
 
 function run() {
-    chrome.tabs.executeScript(ly_tab,{
-        code: "var btn = document.getElementById('disconnect');btn.click();"
-    });
-    setTimeout("lianjie()",5000);
+    if (ips.length > 0) {
+        var ip = ips.pop();
+        var row = ip.split(":");
+        if(row.length === 2) {
+            console.log(ip);
+            set_proxy(row[0], row[1]);
+            chrome.cookies.getAll({url:"http://www.hao123.com/?tn=90384165_hao_pg"}, function(cookies) {
+                for(var i = 0; i < cookies.length; i++) {
+                    var cookie = cookies[i];
+                    chrome.cookies.remove({url:"http://www.hao123.com/?tn=90384165_hao_pg", name: cookie.name}, function() {});
+                }
+            });
+            chrome.tabs.update(cur_tab, {url: "http://www.hao123.com/?tn=90384165_hao_pg"}, function() {});
+        }
+    } else {
+        get_ips();
+    }
 }
 
-function lianjie() {
-    chrome.tabs.executeScript(ly_tab,{
-        code: "var btn = document.getElementById('save');btn.click();"
-    });
+function set_proxy(ip, port) {
+    var config = {
+        mode: "fixed_servers",
+        rules: {
+            singleProxy: {
+                host: ip,
+                port: parseInt(port)
+            },
+            bypassList: ["*mogumiao.com"]
+        }
+    };
+    chrome.proxy.settings.set(
+        {value: config, scope: 'regular'},
+        function() {});
 }
 
-function reload() {
-    if(cur_tab !== null)
-        chrome.tabs.remove(cur_tab, function(){});
-    chrome.tabs.create({url: "http://www.hao123.com/?tn=90384165_hao_pg"}, function(tab){
-        cur_tab = tab.id;
+function use_system_proxy() {
+    var config = {
+        mode: "direct"
+    };
+    chrome.proxy.settings.set(
+        {value: config, scope: 'regular'},
+        function() {});
+}
+
+function get_ips() {
+    use_system_proxy();
+    $.get(api_url, function(result){
+        var arrayOfLines = result.match(/[^\r\n]+/g);
+        for(var i = 0; i < arrayOfLines.length; i++) {
+            var line = arrayOfLines[i];
+            if(!line.startsWith('E')){
+                ips.push(line);
+            }
+        }
     });
 }
 
